c5e4c77f3aadb0c56563b17f79fb5fe2ca87a442
James Allsopp <j.allsopp@bham.ac.uk>
2024-11-07 17:23:37 +0000

    Revert "Oops, missed a bit!"
    This reverts commit 8809b4b506dfad7d66086c874583fb8cbd8f2f6b.

--- setup.sh
+++ setup.sh
@@ -237,13 +237,13 @@ chmod +x ~/${CRON_SCRIPT_NAME}
 path_to_environment_variables="./CodeToRun/environment_variables.sh"
 
 if [[ -v USE_SCRON ]]; then
-    CRON_EXECUTABLE="scrontab"
+    CRON_EXECUTABLE="scrontab -l"
 else
-    CRON_EXECUTABLE="crontab"
+    CRON_EXECUTABLE="crontab -l"
 fi
 
 #This attempts to not clobber existing scron, whilst being functional idempto....
-original_cron=$(${CRON_EXECUTABLE} -l)
+original_cron=$(CRON_EXECUTABLE)
 new_cron=$(echo "${original_cron}" | sed 's/'^.*${CRON_SCRIPT_NAME}.*$'//g')
 
 if [[ -v USE_SCRON ]]; then
@@ -311,7 +311,7 @@ done
 
 echo -e "${warning}" > "${flow_cron_config_dir}/$(date +%FT%T)_${CRON_SCRIPT_NAME}"
 
-echo "${new_cron}" | ${CRON_EXECUTABLE} -
+echo "${new_cron}" | crontab -
 set +o noglob
 
 #create file in home directory to execute, can't directly do this due to permissions in CRON.
